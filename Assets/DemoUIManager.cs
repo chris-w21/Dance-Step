e29e659e86876055467ea5eb47e65fdfb9fe7391
chrisware-21 <103966383+chris-w21@users.noreply.github.com>
2022-06-21 05:14:53 +0500

    Implemented all UI Checkpoints

--- Assets/DemoUIManager.cs
+++ Assets/DemoUIManager.cs
@@ -1,20 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
 
 public class DemoUIManager : MonoBehaviour
 {
+    [SerializeField] private Toggle[] instrumentToggles;
+
+    [SerializeField] private Transform instrumentTogglesContent;
+
+    [SerializeField] private Toggle instrumentTogglePrefab;
+
     public Type type;
     public Role role;
     public Timing timing;
     public BPM bpm;
 
+    public Toggle toggle;
+
     public Footwork[] footworks;
 
     private int selectedFootwork;
 
+    private bool isPlaying = false;
+
+    private void Start()
+    {
+        UpdateToggles();
+    }
+
+    public void ChangeType(int i)
+    {
+        type = (Type)i;
+        OnAnyDropDownChanged();
+    }
+
+    public void ChangeRole(int i)
+    {
+        role = (Role)i; 
+        OnAnyDropDownChanged();
+    }
+
+    public void ChangeTiming(int i)
+    {
+        timing = (Timing)i;
+        OnAnyDropDownChanged();
+    }
+
+    public void ChangeBPM(int i)
+    {
+        bpm = (BPM)i;
+        OnAnyDropDownChanged();
+    }
+
     public void OnAnyDropDownChanged()
     {
+
+        footworks[selectedFootwork].Stop();
+        footworks[selectedFootwork].gameObject.SetActive(false);
         switch (type)
         {
             case Type.Basic:
@@ -94,7 +139,7 @@ public class DemoUIManager : MonoBehaviour
                     default:
                         switch (timing)
                         {
-                            case Timing.On1:
+                            default:
                                 switch (bpm)
                                 {
                                     case BPM.BPM80:
@@ -114,31 +159,86 @@ public class DemoUIManager : MonoBehaviour
                 break;
         }
 
-        for (int i = 0; i < footworks.Length; i++)
-        {
-            footworks[i].Stop();
-            footworks[i].gameObject.SetActive(false);
-        }
+        //for (int i = 0; i < footworks.Length; i++)
+        //{
+        //    footworks[i].Stop();
+        //    footworks[i].gameObject.SetActive(false);
+        //}
+
         footworks[selectedFootwork].gameObject.SetActive(true);
-        footworks[selectedFootwork].Play();
+        UpdateToggles();
+        toggle.isOn = true;
+    }
+
+    private void UpdateToggles()
+    {
+        for (int i = 0; i < instrumentToggles.Length; i++)
+        {
+            if (instrumentToggles[i])
+            {
+                Destroy(instrumentToggles[i].gameObject);
+            }
+        }
+
+        AudioSource[] sources = footworks[selectedFootwork].GetComponents<AudioSource>();
+
+        instrumentToggles = new Toggle[sources.Length];
+        for (int i = 0; i < instrumentToggles.Length; i++)
+        {
+            if (sources[i].volume != 0f)
+            {
+                instrumentToggles[i] = Instantiate(instrumentTogglePrefab, instrumentTogglesContent);
+                var a = i;
+                sources[i].volume = instrumentToggles[i].isOn ? 1 : 0;
+                instrumentToggles[i].onValueChanged.AddListener(delegate
+                {
+                    ToggleInstrument(instrumentToggles[a].isOn, sources[a]);
+                });
+                instrumentToggles[i].GetComponentInChildren<TextMeshProUGUI>().text = sources[i].clip.name.Remove(0, 6);
+            }
+        }
+    }
+
+    private void ToggleInstrument(bool isOn, AudioSource source)
+    {
+        source.volume = isOn ? 1 : 0;
+    }
+
+    public void TogglePlaying()
+    {
+        isPlaying = !isPlaying;
+        if (isPlaying)
+        {
+            footworks[selectedFootwork].Play();
+            isPlaying = true;
+        }
+        else
+        {
+            footworks[selectedFootwork].Pause();
+            isPlaying = false;
+        }
     }
 }
 
+[System.Serializable]
 public enum Type
 {
     Basic, SideBasic
 }
 
+[System.Serializable]
 public enum Role
 {
     Lead, Follow
 }
 
+[System.Serializable]
 public enum Timing
 {
     On1, On2
 }
 
+[System.Serializable]
 public enum BPM
 {
     BPM80, BPM90, BPM100
